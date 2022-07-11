7c82e897efb073f3de6279e8acb1741043837cd3
Nandhini K <90173983+NandhiniK-27@users.noreply.github.com>
2022-07-12 04:47:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/config/routes.rb
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config/routes.rb
+" > ~/.streamlit/config.toml
